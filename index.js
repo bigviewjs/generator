e47ac1601ae226e0a8c8dc085a287bd725d392b5
狼叔 <langshu.ssl@alibaba-inc.com>
2018-03-09 17:32:34 +0800

    lazycommmit, please ignore

--- index.js
+++ index.js
@@ -3,8 +3,26 @@ var ykit = require("ykit")
 
 console.log(ykit.commands.server)
 
+var proxy = require('http-proxy-middleware');
+
+/**
+ * Configure proxy middleware
+ */
+var jsonPlaceholderProxy = proxy(
+    {
+        target: 'http://127.0.0.1:8002',
+        changeOrigin: true,             // for vhosted sites, changes host header to match to target's host
+        logLevel: 'debug',
+        pathRewrite: {
+            '^/api/*': '/',     // rewrite path
+        },
+    }
+)
+
 ykit.commands.server.run({
     cwd: process.cwd(),
-    p: 8090
+    p: 8090,
+    apis: {
+        '/api': jsonPlaceholderProxy
+    }
 })
-
