b32dcb20c13db95139f66e98c7ac093c4de3f195
年翼 <nianyi.zxy@alibaba-inc.com>
2018-03-13 16:25:51 +0800

    fix: 修改方法名称

--- template/app/bpmodule/main/lib/fetch.js
+++ template/app/bpmodule/main/lib/fetch.js
@@ -10,7 +10,7 @@ module.exports = async function fetch () {
   // simulation fetch time
   await _timeout(2000)
 
-  const logo = 'http://i4.bvimg.com/635848/6b04ad113d1d828a.png'
+  const logo = 'http://i2.bvimg.com/635848/4ee74502d03da0ca.png'
   const headline = 'Main'
 
   // simulation data
